3f7d2dcb60850ca330d48c52c86a6479f7dad704
ranjansingh763-bit <ranjansingh763@gmail.com>
2026-01-16 23:10:12 +0530

    Create projrct2.py

---
+++ projrct2.py
@@ -0,0 +1,9 @@
+# sales = [1200,1500,900,1800,1600]
+# total_sales = sum(sales)
+# print(total_sales)
+# avg_sales = total_sales/5
+# print(avg_sales)
+# highest_sales = max(sales)
+# print(highest_sales)
+# lowest_sales = min(sales)
+# print(lowest_sales)
